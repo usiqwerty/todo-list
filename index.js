ff41b37537374f0f6f6a770ca38f93520cbd069a
yura <yura.romashov000@mail.ru>
2025-05-13 16:03:53 +0500

    task 3

--- index.js
+++ index.js
@@ -1,92 +1,115 @@
 ﻿function createElement(tag, attributes, children, onClick, onChange) {
-  const element = document.createElement(tag);
+    const element = document.createElement(tag);
 
-  if (attributes) {
-    Object.keys(attributes).forEach((key) => {
-      element.setAttribute(key, attributes[key]);
-    });
-  }
+    if (attributes) {
+        Object.keys(attributes).forEach((key) => {
+            element.setAttribute(key, attributes[key]);
+        });
+    }
 
-  if (Array.isArray(children)) {
-    children.forEach((child) => {
-      if (typeof child === "string") {
-        element.appendChild(document.createTextNode(child));
-      } else if (child instanceof HTMLElement) {
-        element.appendChild(child);
-      }
-    });
-  } else if (typeof children === "string") {
-    element.appendChild(document.createTextNode(children));
-  } else if (children instanceof HTMLElement) {
-    element.appendChild(children);
-  }
-  if (onClick !== undefined){
-    element.addEventListener('click', onClick);
-  }
-  if (onChange !== undefined){
-    element.addEventListener('change', onChange);
-  }
-  return element;
+    if (Array.isArray(children)) {
+        children.forEach((child) => {
+            if (typeof child === "string") {
+                element.appendChild(document.createTextNode(child));
+            } else if (child instanceof HTMLElement) {
+                element.appendChild(child);
+            }
+        });
+    } else if (typeof children === "string") {
+        element.appendChild(document.createTextNode(children));
+    } else if (children instanceof HTMLElement) {
+        element.appendChild(children);
+    }
+    if (onClick !== undefined) {
+        element.addEventListener('click', onClick);
+    }
+    if (onChange !== undefined) {
+        element.addEventListener('change', onChange);
+    }
+    return element;
 }
 
 class Component {
-  constructor() {
-  }
+    constructor() {
+        this._domNode = null;
+    }
 
-  getDomNode() {
-    this._domNode = this.render();
-    return this._domNode;
-  }
+    getDomNode() {
+        this._domNode = this.render();
+        return this._domNode;
+    }
+
+    update() {
+        const newDomNode = this.render();
+        this._domNode.replaceWith(newDomNode);
+        this._domNode = newDomNode;
+    }
 }
 
 class TodoList extends Component {
-  state;
-  constructor() {
-    super();
-    this.state = ["Сделать домашку","Сделать практику", "Пойти домой"];
-    this.currentInputValue = "";
-  }
-  renderRecursion(state){
-      let children = state.children;
-      if (typeof state.children === 'object') {
-          children = state.children.map(c=>this.renderRecursion(c));
-      }
-      return createElement(state.tag, state.attrs, children, state.onClick, state.onChange);
-  }
-  render() {
-    const elementStructure = {
-      tag: "div", attrs: {class: 'todo-list'}, children: [
-        {tag: 'h1', attrs: {}, children: 'TODO List'},
-        {tag: 'div', attrs: { class: "add-todo" }, children: [
-            {tag: 'input', attrs: {id: 'new-todo', type: 'text', placeholder: 'Задание'}, onChange: this.onAddInputChange.bind(this)},
-            {tag:"button", attrs: { id: "add-btn" }, children: "+", onClick: this.onAddTask.bind(this)}
-          ]
-        },
-        {tag: 'ul', attrs: {id:'todos'}, children: this.state.map(
-              label => ({tag: 'li', attrs: {}, children: [
-                  {tag: 'input', attrs:{type:'checkbox'}},
-                  {tag: 'label', attrs:{}, children: label},
-                  {tag: 'button', attrs:{}, children: '🗑️', onClick:
-                        ()=> {
-                          this.state = this.state.filter(x => x !== label);
-                          console.log(this.state)
-                        }},
-                ]})
-          )}
-      ]
-    };
-    return this.renderRecursion(elementStructure);
-  }
-  onAddTask(){
-      this.state.push(this.currentInputValue);
-      console.log(this.state)
-  }
-  onAddInputChange(e){
-    this.currentInputValue = e.target.value;
-  }
+    constructor() {
+        super();
+        this.state = ["Сделать домашку", "Сделать практику", "Пойти домой"];
+        this.currentInputValue = "";
+    }
 
+    renderRecursion(state) {
+        let children = state.children;
+        if (typeof state.children === 'object') {
+            children = state.children.map(c => this.renderRecursion(c));
+        }
+        return createElement(state.tag, state.attrs, children, state.onClick, state.onChange);
+    }
+
+    render() {
+        const elementStructure = {
+            tag: "div", attrs: {class: 'todo-list'}, children: [
+                {tag: 'h1', attrs: {}, children: 'TODO List'},
+                {
+                    tag: 'div', attrs: {class: "add-todo"}, children: [
+                        {
+                            tag: 'input',
+                            attrs: {id: 'new-todo', type: 'text', placeholder: 'Задание', value: this.currentInputValue},
+                            onChange: this.onAddInputChange.bind(this)
+                        },
+                        {tag: "button", attrs: {id: "add-btn"}, children: "+", onClick: this.onAddTask.bind(this)}
+                    ]
+                },
+                {
+                    tag: 'ul', attrs: {id: 'todos'}, children: this.state.map(
+                        label => ({
+                            tag: 'li', attrs: {}, children: [
+                                {tag: 'input', attrs: {type: 'checkbox'}},
+                                {tag: 'label', attrs: {}, children: label},
+                                {
+                                    tag: 'button', attrs: {}, children: '🗑️', onClick:
+                                        () => {
+                                            this.state = this.state.filter(x => x !== label);
+                                            this.update();
+                                        }
+                                },
+                            ]
+                        })
+                    )
+                }
+            ]
+        };
+        return this.renderRecursion(elementStructure);
+    }
+
+    onAddTask() {
+        if (this.currentInputValue.trim()) {
+            this.state.push(this.currentInputValue);
+            this.currentInputValue = "";
+            this.update();
+        }
+    }
+
+    onAddInputChange(e) {
+        this.currentInputValue = e.target.value;
+    }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  document.body.appendChild(new TodoList().getDomNode());
+    document.body.appendChild(new TodoList().getDomNode());
 });
